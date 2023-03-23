ab54ca12f1d3bcb152f0b109078abc4d140112bc
Seemal Tausif <24100024@lums.edu.pk>
2023-03-23 22:38:13 +0500

    events popup

--- client/src/helpers/EventList.js
+++ client/src/helpers/EventList.js
@@ -7,22 +7,30 @@ export const EventList = [
   {
     name: "Plantation Drive",
     image: Plantation,
+    // image: "plantation.webp",
     text: "Paktree holds monthly donation drives",
+    details: "njrevckbencnvjecjdbrbejbmdhvbiughjgfbjdsbfjdbufhgfbjdbvfhkegbfjkdbcjkbdkvjbjdsbjkvbkjbvkjfjkvbfjkdbvkjfdbkjvfbvdf",
   },
   {
     name: "Clothing Drive",
     image: Clothes,
+    // image: "clothes.webp",
     text: "Paktree holds monthly donation drives",
+    details: "njrevckbencnvjecjdbrbejbmdhvbiughjgfbjdsbfjdbufhgfbjdbvfhkegbfjkdbcjkbdkvjbjdsbjkvbkjbvkjfjkvbfjkdbvkjfdbkjvfbvdf",
   },
   {
     name: "Food Drive",
     image: Food,
+    // image: "food.jpg",
     text: "Paktree holds monthly donation drives",
+    details: "njrevckbencnvjecjdbrbejbmdhvbiughjgfbjdsbfjdbufhgfbjdbvfhkegbfjkdbcjkbdkvjbjdsbjkvbkjbvkjfjkvbfjkdbvkjfdbkjvfbvdf",
   },
   {
     name: "Clean Water Campaign",
     image: Water,
+    // image: "water.jpg",
     text: "Paktree holds monthly water campaigns",
+    details: "njrevckbencnvjecjdbrbejbmdhvbiughjgfbjdsbfjdbufhgfbjdbvfhkegbfjkdbcjkbdkvjbjdsbjkvbkjbvkjfjkvbfjkdbvkjfdbkjvfbvdf",
   }
   
 ];
