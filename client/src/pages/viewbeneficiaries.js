b68029741cc2448f209bfc842b8b5043b4440a46
Zoha Hayat Bhatti <24100010@lums.edu.pk>
2023-04-03 13:11:54 +0500

    few edits

--- client/src/pages/viewbeneficiaries.js
+++ client/src/pages/viewbeneficiaries.js
@@ -6,6 +6,9 @@ import "../styles/viewbeneficiaries.css"
 import { useState,useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
+import Button from "@mui/material/Button";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { IconButton } from '@mui/material';
 
 const ViewBeneficiaries=()=> {
 
@@ -29,22 +32,37 @@ const ViewBeneficiaries=()=> {
             cnic: cnic
         }
         ).then((res) => {
-            navigate("/directorPage/viewbeneficiaries");
+            navigate("/directorPage/addbeneficiary");
         }).catch(err => {console.log(err)})
 
     }
 
+    const navigateToAddbene = () => {
+      navigate('/directorPage/addstory')
+  }
+
     return (
-        <div className='Benefit_'>
-          <div className='navbar2'>
-                 <h1 className = "BenefitHeading">Beneficiaries</h1>
-                 <div className='leftSide'>
-                     <h3>Add New</h3>
-                     <Link to ="/directorPage/addbeneficiary"><img src={circle}/></Link>
-                 </div>
-          </div>
-        {/* <h1 className = "BenefitHeading">Beneficiaries</h1> */}
-        <div className='horizontal_line'></div>
+      
+        // <div className='Benefit_'>
+        //   <div className='navbar2'>
+        //          <h1 className = "BenefitHeading">Beneficiaries</h1>
+        //          <div className='leftSide'>
+        //              <h3>Add New</h3>
+        //              <Link to ="/directorPage/addbeneficiary"><img src={circle}/></Link>
+        //          </div>
+        //   </div>
+        // <div className='horizontal_line'></div>
+        <div className = "Benefit_">
+            <div className='above_hor_line'>
+            <h1 className = "Storyheading_">Beneficiaries</h1>
+            <div className='stor_button_new_text'>
+            <h2 className = "Storysubheading">Add New</h2>
+                <IconButton  sx={{display: "flex", justifyContent: "flex-end"}} onClick={navigateToAddbene}>
+                    <AddCircleIcon style={{color: "#92D4D2", fontSize:60}}/>
+                </IconButton>
+                </div>
+                </div>
+            <div className='horizontal_line'></div>
         <div className="benefitList">
             {
               benefit.map((val) => {
