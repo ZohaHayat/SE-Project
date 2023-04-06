720e2ddc105a163a73f6c2dd1ac0719b7cba63db
Zoha Hayat Bhatti <24100010@lums.edu.pk>
2023-04-06 21:33:12 +0500

    fixed reload

--- client/src/pages/viewVolunteers.js
+++ client/src/pages/viewVolunteers.js
@@ -23,25 +23,51 @@ const ViewVolunteers=()=> {
     })
 
     return (
-        <div className = "Volunteers">
-            <h1 className = "Volunteerheading">Volunteers</h1>
-            {volunteers.map((val, key) => { 
+        <div className='Members_'>
+        <h1 className = "memHeading">Volunteers</h1>
+        <div className='horizontal_line'></div>
+        <div className="memList">
+            {/* <div className='horizontal_line'></div> */}
+
+            {
+              volunteers.map((val) => {
                 return (
-                    <div className="flex-container">
-                        <div class="flex-child-magenta">
-                            <img className="volunteer-img" src={Logo}/>
-                        </div>
-                        <div class="flex-child-green">
-                            <h3>{val.Name}</h3>
-                            <h4>{val.Event}</h4>
-                         </div>
+                    <div className='memItem'>
+                  <div className="memImg">
+                    <img src={Icon} alt=""/>
+                    </div>
+                    <div className="memText">
+                    <h2>{val.Name}</h2>
+                    <p>{val.Event}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+
+
+    </div>
+        // <div className = "Volunteers">
+        //     <h1 className = "Volunteerheading">Volunteers</h1>
+        //     {volunteers.map((val, key) => { 
+        //         return (
+        //             <div className="flex-container">
+        //                 <div class="flex-child-magenta">
+        //                     <img className="volunteer-img" src={Logo}/>
+        //                 </div>
+        //                 <div class="flex-child-green">
+        //                     <h3>{val.Name}</h3>
+        //                     <h4>{val.Event}</h4>
+        //                  </div>
                     
-                    </div>)
-            })}
+        //             </div>)
+        //     })}
           
 
            
-        </div>
+        // </div>
       )
 }
 
