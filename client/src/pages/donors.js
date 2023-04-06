69d3c11bd8c9e92281eb9ae4c0ec512334aa6de9
Zoha Hayat Bhatti <24100010@lums.edu.pk>
2023-04-06 21:29:22 +0500

    fixed reload

--- client/src/pages/donors.js
+++ client/src/pages/donors.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Axios from "axios"
 import "../styles/donors.css"
 import { useState, useEffect } from "react";
-import PersonAvatar from '../assets/person-icon.png'
+import PersonAvatar from '../assets/nodp.png'
 
 function Donors() {
     
@@ -18,20 +18,47 @@ function Donors() {
     
 
     return (
-        <div className='flex-container'>
-            <h1 className='heading' >Donors</h1>
-            {donors.map((val, key) => { 
+        <div className='Members_'>
+        <h1 className = "memHeading">Donors</h1>
+        <div className='horizontal_line'></div>
+        <div className="memList">
+            {/* <div className='horizontal_line'></div> */}
+
+            {
+              donors.map((val) => {
                 return (
-                    <div className='donor-div' key={val.Name}>
-                        <img className='donor-avatar' src={PersonAvatar}/>
-                        <div className='donor-details-div'>
-                            <div className='donor-name-div'> {val.Name} </div> 
-                            <div> Donated Rs. {val.DonationAmount} </div>
-                        </div>
-                    </div>)
-            })}
+                    <div className='memItem'>
+                  <div className="memImg">
+                    <img src={PersonAvatar} alt=""/>
+                    </div>
+                    <div className="memText">
+                    <h2>{val.Name}</h2>
+                    <p>Donated Rs. {val.DonationAmount}</p>
+                    </div>
+                  </div>
+                )
+              })
+            }
+
+          </div>
+
+
+    </div>
+
+        // <div className='flex-container'>
+        //     <h1 className='heading' >Donors</h1>
+        //     {donors.map((val, key) => { 
+        //         return (
+        //             <div className='donor-div' key={val.Name}>
+        //                 <img className='donor-avatar' src={PersonAvatar}/>
+        //                 <div className='donor-details-div'>
+        //                     <div className='donor-name-div'> {val.Name} </div> 
+        //                     <div> Donated Rs. {val.DonationAmount} </div>
+        //                 </div>
+        //             </div>)
+        //     })}
             
-        </div>
+        // </div>
       )
 }
 
